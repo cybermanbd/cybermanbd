b69a2b14faf96ed41f3e41f35a02ee360c08a201
Sheikh Abujar <cybermanbd@gmail.com>
2015-11-23 16:19:12 +0600

    Test 2

---
+++ ClassAccessing/src/Class3.java
@@ -0,0 +1,14 @@
+
+public class Class3 {
+	public static int class3()
+	{
+		mainClass oMainClass = new mainClass();
+		System.out.println("Class 3 Present");
+		//oMainClass.main(null);
+		System.out.println("Sending Class 2 with value 1 ");
+		return 1;
+		
+	}
+
+
+}
---
+++ ClassAccessing/src/mainClass.java
@@ -0,0 +1,16 @@
+
+public class mainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Class2 oClass2 = new Class2();
+		Class3 oClass3 = new Class3();
+		System.out.println("Main Class Initiating ");
+		
+		int class1 = oClass2.class2();
+		System.out.println(class1);
+		
+
+	}
+
+}
